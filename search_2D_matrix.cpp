f9d698ac91c517f5c3947078a075e4e9b7d8174d
Shivansh Kush <kushivansh27@gmail.com>
2024-03-15 17:42:36 +0530

    problem 3

--- search_2D_matrix.cpp
+++ search_2D_matrix.cpp
@@ -7,7 +7,7 @@ Logic:
     - Initial a low value to 0 and high to last index
     - Find a mid value by taking average of high and low
     - Find the element at mid value index by using this formula: matrix[mid / column][mid % column]
-    - If the element is < target then low = mid + 1, else high = mid - 1  
+    - If the element is < target then low = mid + 1, else high = mid - 1
 */
 
 #include <bits/stdc++.h>
@@ -37,6 +37,55 @@ int main()
 {
     vector<vector<int>> matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
     int target = 3;
-    searchMatrix(matrix, target) ? cout << "true" << endl : cout << "false" << endl;    
+    searchMatrix(matrix, target) ? cout << "true" << endl : cout << "false" << endl;
+    return 0;
+}
+
+// https://www.geeksforgeeks.org/search-in-row-wise-and-column-wise-sorted-matrix/
+
+/*
+Logic:
+    - If both rows and columns are sorted
+    - Place the pointer at last column of first row
+    - If the element at the pointer is less than target increase the row value else decrease the column value
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int searchSortedMatrix(vector<vector<int>> &matrix, int target)
+{
+    int n = matrix.size();
+    int j = matrix[0].size() - 1;
+    if (n == 0)
+        return -1;
+    int i = 0;
+    while (i < n && j >= 0)
+    {
+        if (matrix[i][j] == target)
+        {
+            cout << "Found " << target << " at " << i << "," << j << endl;
+            return 1;
+        }
+        if (matrix[i][j] < target)
+        {
+            i++;
+        }
+        else
+        {
+            j--;
+        }
+    }
+    cout << "Element not found" << endl;
+    return 0;
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{10, 20, 30, 40},
+                                  {15, 25, 35, 45},
+                                  {27, 29, 37, 48},
+                                  {32, 33, 39, 50}};
+    int target = 25;
+    searchSortedMatrix(matrix, target);
     return 0;
 }
